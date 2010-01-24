5081a46fe170d2a0e9ad567ab72812cceb0f8808
thinfox <thinfox@dac0a092-8d60-11dd-b496-dd7b60ab8c28>
2010-01-24 12:55:35 +0000

    adds the program menu to its own "Kasahorow" folder so it behaves properly under vista and windows 7
    git-svn-id: http://africakeyboards.googlecode.com/svn/trunk@55 dac0a092-8d60-11dd-b496-dd7b60ab8c28

--- Kasahorow.KeyboardInstaller/Kasahorow.KeyboardInstaller/Properties/AssemblyInfo.cs
+++ Kasahorow.KeyboardInstaller/Kasahorow.KeyboardInstaller/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Kasahorow.KeyboardInstaller")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Keyboard installer application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Kasahorow.org")]
 [assembly: AssemblyProduct("Kasahorow.KeyboardInstaller")]
 [assembly: AssemblyCopyright("Copyright ©  2009")]
 [assembly: AssemblyTrademark("")]
